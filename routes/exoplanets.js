0c33d215a1198329bfaedc8d7fa6954943bd956e
Aya ELk <aya.elhakkouni@student.vinci.be>
2022-12-19 05:22:33 +0100

    restart

--- routes/exoplanets.js
+++ routes/exoplanets.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const Exoplanet = require('../models/Exoplanet.js');
 
+
+const Exoplanet = require('../models/Exoplanet.js');
 
 /* GET exoplanets index. */
 router.get('/', (req, res, next) => {
@@ -11,12 +12,12 @@ router.get('/', (req, res, next) => {
 
 /* POST add exoplanet. */
 router.post('/add', (req, res, next) => {
-  console.log('POST ADD EXOPLANET' + req.body.uniqueNameExoplanet);
-    Exoplanet.save({
-      uniqueName: req.body.uniqueNameExoplanet,
-      hClass: req.body.hClassExoplanet,
-      discoveryYear: req.body.discoveryYearExoplanet
-    });
+  console.log("POST ADD EXOPLANET");
+  Exoplanet.save({
+    uniqueName: req.body.uniqueNameExoplanet,
+    hClass: req.body.hClassExoplanet,
+    discoveryYear: req.body.discoveryYearExoplanet
+  });
   res.redirect('/exoplanets');
 });
 
@@ -24,15 +25,14 @@ router.post('/add', (req, res, next) => {
 router.get('/search', (req, res, next) => {
   let exoplanetsTable = null;
   let min3Char = false;
-  console.log('GET SEARCH EXOPLANET');
+  console.log("GET SEARCH EXOPLANET");
   if (req.query.uniqueNameExoplanet.length >= 3) {
     min3Char = true;
     exoplanetsTable = Exoplanet.search(req.query.uniqueNameExoplanet);
   }
-  res.render('exoplanets/index', {
-    exoplanetsTable,
-    min3Char
-  });
+  res.render('exoplanets/index', { exoplanetsTable: exoplanetsTable, min3Char: min3Char });
 });
+
+
 
 module.exports = router;
